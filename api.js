d817e1095bc33e46ad104fbe63093897625458ac
Rohan-mgr <rohan.magar.415@gmail.com>
2024-07-20 21:32:28 +0545

    fix api request issue

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ axios.defaults.baseURL = "https://api.openweathermap.org/data/2.5/";
 export async function getCoordinates(cityName) {
   try {
     let response = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=80514d89abfbae865491be2e2d570c7f`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=80514d89abfbae865491be2e2d570c7f`
     );
     if (response?.status !== 200 || response?.statusText !== "OK") {
       throw new Error("Failed to forecast weather");
